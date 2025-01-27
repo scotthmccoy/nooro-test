859e83c9a4fc7df482fb6d7c994cf48aa1476045
Scott McCoy <scotthmccoy@gmail.com>
2025-01-27 12:31:53 -0800

    Initial commit

---
+++ Weather/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by Scott McCoy on 1/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Scott McCoy on 1/27/25.
+//
+
+import Testing
+@testable import Weather
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
